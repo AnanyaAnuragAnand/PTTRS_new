c72df25565b6230b4c3f7ee63679dfe31bf66a33
Ananya Anurag Anand <87889864+AnanyaAnuragAnand@users.noreply.github.com>
2025-09-09 21:22:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import warnings
-warnings.filterwarnings("ignore", category=SyntaxWarning)
-
 import streamlit as st, mysql.connector as mysql
 import reset, account_control, home, account_details, developers_info, contacts
 from streamlit_option_menu import option_menu
